2c1c7efadd78184de80d0e43d517ed8cea40cbb9
SuperstitiousSeiya <115018979+SuperstitiousSeiya@users.noreply.github.com>
2023-08-08 14:06:55 +0800

    Add files via upload
    added new batch of html and updates

--- Website/links/Faqs/faqs.js
+++ Website/links/Faqs/faqs.js
@@ -1,31 +1,27 @@
 function toggleAnswer(flipper) {
-    flipper.parentElement.classList.toggle('flipped');
+  const response = flipper.nextElementSibling;
+  const angleDown = flipper.querySelector('.fa-angle-down');
+  const angleUp = flipper.querySelector('.fa-angle-up');
+
+  response.style.display = (response.style.display === 'none') ? 'block' : 'none';
+  angleDown.style.display = (response.style.display === 'none') ? 'block' : 'none';
+  angleUp.style.display = (response.style.display === 'none') ? 'none' : 'block';
 }
-function ToggleAnswer(button) {
-    const response = button.parentElement.parentElement.nextElementSibling;
-    response.style.display = (response.style.display === 'none') ? 'block' : 'none';
 
-    const plusButton = button.parentElement.querySelector('.fa-plus');
-    const minusButton = button.parentElement.querySelector('.fa-minus');
-
-    plusButton.style.display = (response.style.display === 'none') ? 'block' : 'none';
-    minusButton.style.display = (response.style.display === 'none') ? 'none' : 'block';
-
-}
 const hideMenuIcon = document.getElementById("hide-menu");
 hideMenuIcon.style.display = "none";
 
 function toggleMenu() {
-    const showMenuIcon = document.getElementById("show-menu");
-    const hideMenuIcon = document.getElementById("hide-menu");
-    const menuItems = document.querySelectorAll("a, .dropdown, .theme");
-    if (showMenuIcon.style.display !== "none") {
-        showMenuIcon.style.display = "none";
-        hideMenuIcon.style.display = "block";
-        menuItems.forEach(item => item.style.display = "block");
-    } else {
-        hideMenuIcon.style.display = "none";
-        showMenuIcon.style.display = "block";
-        menuItems.forEach(item => item.style.display = "none");
-    }
+  const showMenuIcon = document.getElementById("show-menu");
+  const hideMenuIcon = document.getElementById("hide-menu");
+  const menuItems = document.querySelectorAll("a, .dropdown, .theme");
+  if (showMenuIcon.style.display !== "none") {
+    showMenuIcon.style.display = "none";
+    hideMenuIcon.style.display = "block";
+    menuItems.forEach(item => item.style.display = "block");
+  } else {
+    hideMenuIcon.style.display = "none";
+    showMenuIcon.style.display = "block";
+    menuItems.forEach(item => item.style.display = "none");
+  }
 }
